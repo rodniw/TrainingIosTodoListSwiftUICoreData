77ab87e6acde011d7b51702568f2b84eb412cf6d
Rodion <rodion.prosvirnin@bk.ru>
2020-07-23 01:12:44 +0300

    rebase

--- TrainingIosTodoListSwiftUICoreData/ContentView.swift
+++ TrainingIosTodoListSwiftUICoreData/ContentView.swift
@@ -6,11 +6,36 @@
 //  Copyright © 2020 Rodion Prosvirnin. All rights reserved.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(fetchRequest: ToDoItem.getAllToDoItems()) var toDoItems: FetchedResults<ToDoItem>
+    
+    @State private var newToDoItem = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List{
+                Section(header: Text("Новая запись")) {
+                    HStack {
+                        TextField("Начните ввод", text: self.$newToDoItem)
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.green)
+                                .imageScale(.large)
+                        }
+                    }
+                }.font(.headline)
+            }
+    
+            .navigationBarTitle(Text("ToDo list"))
+            .navigationBarItems(trailing: EditButton())
+        }
     }
 }
 
---
+++ TrainingIosTodoListSwiftUICoreData/ToDoItem.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoItem.swift
+//  TrainingIosTodoListSwiftUICoreData
+//
+//  Created by Rodion Prosvirnin on 23.07.2020.
+//  Copyright © 2020 Rodion Prosvirnin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class ToDoItem: NSManagedObject, Identifiable {
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var title: String?
+}
+
+extension ToDoItem {
+    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
+        let request: NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as! NSFetchRequest<ToDoItem>
+        
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
